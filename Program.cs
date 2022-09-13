efcb5a15d02d7a27e61c67fddb4fbc36faafa51d
Gustavo Ariel Barrajón <92478917+GuArBarrajon@users.noreply.github.com>
2022-09-13 17:17:16 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,102 +1,14 @@
-﻿/*
- Alumno: Gustavo Barrajón
-Turno Tarde
-Ejercicio 1
-Escribe un programa que tome 3 números enteros introducidos por el usuario mediante el teclado y determine cuántos de dichos 
-números son diferentes, el promedio, la suma de todos, el producto del mayor por el menor y si el número restante es divisible 
-por 3.
- */
+﻿Punto P1 = new Punto();
+Punto P2 = new Punto();
 
-static void Diferentes(int n1, int n2, int n3)
-{
-    if (n1 == n2 && n1 == n3)
-    {
-        Console.WriteLine("Los numeros son todos iguales");
-    }
-    else if (n1 == n2 ^ n1 == n3 ^ n2 == n3)
-    {
-        Console.WriteLine("Hay dos numeros iguales y uno distinto");
-    }
-    else
-    {
-        Console.WriteLine("Son 3 numeros disitntos");
-    }
-}
+P1.SetX(51);
+P1.SetY(63);
+P2.SetX(102);
+P2.SetY(83);
 
-static int Suma(int n1, int n2, int n3)
-{
-    return n1 + n2 + n3;
-}
+P1.IgualA(P2);
 
-static float Promedio(int n1, int n2, int n3)
-{
-    return (n1 + n2 + n3) / 3;    // No pude usar la función Suma (Una función estática no puede contener una referencia a "this" o "base")
-}
-
-static void ProdMayorMenoryRestante(int n1, int n2, int n3)
-{
-    int mayor = 0, menor = 0, restante = 0;
-
-    if (n1 > n2 && n1 > n3)
-    {
-        mayor = n1;
-    }
-    else if (n2 > n3)
-    {
-        mayor = n2;
-    }
-    else
-    {
-        mayor = n3;
-    }
-
-    if (n1 < n2 && n1 < n3)
-    {
-        menor = n1;
-    }
-    else if (n2 < n3)
-    {
-        menor = n2;
-    }
-    else
-    {
-        menor = n3;
-    }
-
-    Console.WriteLine("El producto del menor (" + menor + ") por el mayor (" + mayor + ") es: " + menor * mayor);
-
-    if ((n1 == mayor || n1 == menor) && (n2 == mayor || n2 == menor))
-    {
-        restante = n3;
-    }
-    else if (n1 == mayor || n1 == menor)
-    {
-        restante = n2;
-    }
-    else
-    {
-        restante = n1;
-    }
-
-    if (restante % 3 == 0)
-    {
-        Console.WriteLine("El numero restante (" + restante + ") es divisible por 3");
-    }
-    else
-    {
-        Console.WriteLine("El numero restante (" + restante + ") NO es divisible por 3");
-    }
-}
-
-int numero1, numero2, numero3;
-Console.Write("Ingrese el primer numero: ");
-numero1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Ingrese el segundo numero: ");
-numero2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Ingrese el tercer numero: ");
-numero3 = Convert.ToInt32(Console.ReadLine());
-
-Diferentes(numero1, numero2, numero3);
-Console.WriteLine("El promedio de los numeros ingresados es: " + Promedio(numero1, numero2, numero3));
-Console.WriteLine("La suma de los numeros ingresados es: " + Suma(numero1, numero2, numero3));
-ProdMayorMenoryRestante(numero1, numero2, numero3);
+Punto R = P1.SumaPuntos(P2);     
+Console.WriteLine("La suma de los puntos ingresados es " + R.GetX() + ", " + R.GetY());
+Punto R2= P1.RestaPuntos(P2);
+Console.WriteLine("La resa de los puntos ingresados es: " + R2.GetX() + ", " + R2.GetY());
---
+++ Punto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+class Punto
+    {
+    private double x;
+    private double y;
+    
+    public double GetX()
+        { return x; }
+
+    public void SetX(double valor)
+        { this.x = valor; }
+
+    public double GetY()
+    { return y; }
+
+    public void SetY(double valor)
+    { this.y = valor; }
+
+    public void IgualA(Punto OtroPunto)
+    {
+        if(this.x == OtroPunto.GetX() && this.y == OtroPunto.GetY())
+        {
+            Console.WriteLine("Son iguales");
+        }
+        else
+        {
+            Console.WriteLine("Son disitintos");
+        }
+    }
+    public Punto SumaPuntos(Punto OtroPunto)
+    {
+        Punto Resultado = new Punto();
+        Resultado.SetX (this.x + OtroPunto.GetX());
+        Resultado.SetY (this.y + OtroPunto.GetY());
+        return Resultado;
+    }
+    public Punto RestaPuntos(Punto OtroPunto)
+    {
+        Punto Resultado = new Punto();
+        Resultado.SetX(this.x - OtroPunto.GetX());
+        Resultado.SetY(this.y - OtroPunto.GetY());
+        return Resultado;
+    }
+}
+
